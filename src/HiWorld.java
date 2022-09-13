fc7cd9e9a055d904904fd556838ddaf3b78cd9a0
Administrator <chd_ahen@126.com>
2022-09-13 15:47:07 +0800

    123

---
+++ src/HiWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @ProjectName: j2ee
+ * @Package: PACKAGE_NAME
+ * @ClassName: HiWorld
+ * @Author: Administrator
+ * @Description: 测试类
+ * @Date: 2022/9/13 14:15
+ * @Version: 1.0
+ */
+public class HiWorld {
+
+}
